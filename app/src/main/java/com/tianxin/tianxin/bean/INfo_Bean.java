c7c8a3fc76c4125bed32be1f16084d7253eaf2f4
bymaxtodo <303077484@qq.com>
2017-06-22 14:21:10 +0800

    修改接受参数数据类型

--- app/src/main/java/com/tianxin/tianxin/bean/INfo_Bean.java
+++ app/src/main/java/com/tianxin/tianxin/bean/INfo_Bean.java
@@ -6,94 +6,136 @@ package com.tianxin.tianxin.bean;
 public class INfo_Bean {
 
     /**
-     * deviceName : br20140009
-     * currentDate : 2017-04-14T10:49:18Z
-     * temp : 18
-     * do : 19
-     * pH : 13
-     * stir : 80.0
-     * feed : 85.0
-     * acid : 70.0
-     * base : 100.0
-     * ca : 0.0
+     * deviceName : br20140010
+     * currentDate : 2017-06-20T05:19:00Z
+     * temp : 36.7
+     * dO : 200
+     * pH : 7.9
+     * stir : 100
+     * feed : 80
+     * acid : 80
+     * base : 100
+     * ca : 0
      * o2 : 100
-     * n2 : 0.0
-     * co2 : 0.0
+     * n2 : 0
+     * co2 : 0
      */
-    private  String deviceName;
-    private  String currentDate;
-    private  String temp;
-    private  String stir;
-    private  String feed;
-    private  String acid;
-    private  String ca;
-    private  String o2;
-    private  String n2;
-    private  String co2;
-    private String dO;
-    private String pH;
-    private String base;
 
-    public  String getDeviceName(){ return  deviceName; }
-    public  void setDeviceName(String deviceName){ this.deviceName = deviceName; }
+    private String deviceName;
+    private String currentDate;
+    private float  temp;
+    private float  dO;
+    private float  pH;
+    private float  stir;
+    private float  feed;
+    private float  acid;
+    private float  base;
+    private float  ca;
+    private float  o2;
+    private float  n2;
+    private float  co2;
 
-    public  String getCurrentDate(){ return  currentDate;}
+    public String getDeviceName() {
+        return deviceName;
+    }
+
+    public void setDeviceName(String deviceName) {
+        this.deviceName = deviceName;
+    }
+
+    public String getCurrentDate() {
+        return currentDate;
+    }
+
     public void setCurrentDate(String currentDate) {
         this.currentDate = currentDate;
     }
 
-    public String getTemp() {
+    public float getTemp() {
         return temp;
     }
-    public void setTemp(String temp) {
+
+    public void setTemp(float temp) {
         this.temp = temp;
     }
 
-    public String getStir() {
-        return stir;
-    }
-    public void  setStir(String stir){ this.stir = stir; }
-
-    public  String getFeed() { return  feed;}
-    public  void setFeed(String feed) { this.feed = feed; }
-
-    public String getAcid() { return  acid; }
-    public void setAcid(String acid) { this.acid = acid; }
-
-    public String getCa() { return  ca; }
-    public void setCa(String ca) { this.ca = ca; }
-
-
-    public String getDO() {
+    public float getDO() {
         return dO;
     }
-    public void setDO(String dO) { this.dO = dO; }
 
-    public String getPH() {
+    public void setDO(float dO) {
+        this.dO = dO;
+    }
+
+    public float getPH() {
         return pH;
     }
-    public void setPH(String pH) {
+
+    public void setPH(float pH) {
         this.pH = pH;
     }
 
-    public String getO2() {
-        return o2;
-    }
-    public void setO2(String O2) {
-        this.o2 = O2;
+    public float getStir() {
+        return stir;
     }
 
-    public String getN2() { return  n2; }
-    public void  setN2(String n2) { this.n2 = n2; }
+    public void setStir(float stir) {
+        this.stir = stir;
+    }
 
-    public String getCo2() { return  co2; }
-    public void  setCo2(String co2) { this.co2 = co2; }
+    public float getFeed() {
+        return feed;
+    }
 
-    public String getBase() {
+    public void setFeed(float feed) {
+        this.feed = feed;
+    }
+
+    public float getAcid() {
+        return acid;
+    }
+
+    public void setAcid(float acid) {
+        this.acid = acid;
+    }
+
+    public float getBase() {
         return base;
     }
-    public void setBase(String base) {
+
+    public void setBase(float base) {
         this.base = base;
     }
 
+    public float getCa() {
+        return ca;
+    }
+
+    public void setCa(float ca) {
+        this.ca = ca;
+    }
+
+    public float getO2() {
+        return o2;
+    }
+
+    public void setO2(float o2) {
+        this.o2 = o2;
+    }
+
+    public float getN2() {
+        return n2;
+    }
+
+    public void setN2(float n2) {
+        this.n2 = n2;
+    }
+
+    public float getCo2() {
+        return co2;
+    }
+
+    public void setCo2(float co2) {
+        this.co2 = co2;
+    }
 }
